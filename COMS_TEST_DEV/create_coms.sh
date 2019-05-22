0940d93f014088330ec3a6b8fb88a3fc0778bce2
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-05-22 08:38:19 +0200

    Update create_coms.sh

--- COMS_TEST_DEV/create_coms.sh
+++ COMS_TEST_DEV/create_coms.sh
@@ -5,6 +5,7 @@ export VOLUME_PREFIX="TEST"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo mkdir -p -- $HOME/tmp
 sudo cd $HOME/tmp
+TMP_DIR=$HOME/tmp
 
 sudo docker volume create --name $VOLUME_PREFIX-COMS-www-data
 sudo docker volume create --name $VOLUME_PREFIX-COMS-www-config
@@ -32,13 +33,12 @@ sudo iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8344 -j ACCEPT
 
 
 #download DB CONFIG
-sudo wget https://raw.githubusercontent.com/BPMspaceUG/LIAM2/master/sqldump/20190516_dump_liam2_structure_v2_incl_mindata.sql 
+sudo wget https://raw.githubusercontent.com/BPMspaceUG/LIAM2/master/sqldump/20190516_dump_liam2_structure_v2_incl_mindata.sql -P $TMP_DIR
 
-# download git LIAM2 and LIAM2-client
-
-#sudo docker info
+# Start enviroment
 sudo docker-compose up -d -f $DIR/docker-compose.yml
 
+# download git LIAM2 and LIAM2-client directly in the right volume- change owner
 sudo git clone https://github.com/BPMspaceUG/LIAM2.git /var/lib/docker/volumes/TEST-LIAM2-www-data/_data
 sudo git clone https://github.com/BPMspaceUG/LIAM2-Client.git /var/lib/docker/volumes/TEST-LIAM2-CLIENT-www-data/_data
 
@@ -48,11 +48,12 @@ sudo chown -R www-data:www-data /var/lib/docker/volumes/TEST-LIAM2-www-data/_dat
 sudo cp $DIR/LIAM2Client/LIAM2_Client_api.secret.inc.php /var/lib/docker/volumes/TEST-LIAM2-CLIENT-www-data/_data/inc
 sudo chown -R www-data:www-data /var/lib/docker/volumes/TEST-LIAM2-CLIENT-www-data/_data/
 
+# import DB
 sudo echo "sleep 20s until DB is up"
 sudo sleep 20s
 sudo echo "IMPORT DB"
-sudo mysql -u root -pBPMSpaceTEST -h 172.28.1.10 --port 3306 < ./20190516_dump_liam2_structure_v2_incl_mindata.sql
-sudo rm -f ./*.sql*
+sudo mysql -u root -pBPMSpaceTEST -h 172.28.1.10 --port 3306 < $TMP_DIR/20190516_dump_liam2_structure_v2_incl_mindata.sql
+sudo rm -f $DIR/*.sql*
 cd $HOME
 #sudo rm -rf $HOME/tmp
 
