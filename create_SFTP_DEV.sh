ad004c1750934dd95e4be2b043aa45356b1250c1
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2019-05-29 10:23:33 +0200

    Update create_SFTP_DEV.sh

--- create_SFTP_DEV.sh
+++ create_SFTP_DEV.sh
@@ -2,5 +2,5 @@
 docker run \
     -v DEV-LIAM2-www-data:/home/developer/LIAM2Server \
     -v DEV-LIAM2-CLIENT-www-data:/home/developer/LIAM2Client \
-    -p 7070:22 -d atmoz/sftp \
+    -p 2222:22 -d atmoz/sftp \
     developer:pass:1001
