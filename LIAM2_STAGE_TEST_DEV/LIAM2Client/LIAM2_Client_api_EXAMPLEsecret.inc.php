b91b4bc3709da1908c3f29fafef570c59a0070e7
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2019-06-05 11:37:34 +0200

    new token in laim client config

--- LIAM2_STAGE_TEST_DEV/LIAM2Client/LIAM2_Client_api_EXAMPLEsecret.inc.php
+++ LIAM2_STAGE_TEST_DEV/LIAM2Client/LIAM2_Client_api_EXAMPLEsecret.inc.php
@@ -1,4 +1,4 @@
 <?php
 $url="http://localhost:80/api.php";
-$token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjEzMzcsImZpcnN0bmFtZSI6Ik1hY2hpbmUiLCJsYXN0bmFtZSI6Ik1hY2hpbmUifQ.wuQyBfke8r7Mxx_Z8x_C5nFN8bxgadTWpJUvVNHGkAw";
+$token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjEzMzcsImZpcnN0bmFtZSI6Ik1hY2hpbmUiLCJsYXN0bmFtZSI6Ik1hY2hpbmUifQ.wuQyBfke8r7Mxx_Z8x_C5nFN8bxgadTWp$";
 define('AUTH_KEY', 'liam2_key');
