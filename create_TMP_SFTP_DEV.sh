ec5ee4c61542b7fcc396073113b52000054c8936
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-08-19 21:41:39 +0200

    Update create_TMP_SFTP_DEV.sh

--- create_TMP_SFTP_DEV.sh
+++ create_TMP_SFTP_DEV.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run \
+    -v bpmspace_docker:/home/developer/bpmspace_docker \
     -v DEV-LIAM2-www-data:/home/developer/LIAM2Server \
     -v DEV-LIAM2-CLIENT-www-data:/home/developer/LIAM2Client \
     -p 2222:22 -d --name tmpsftpserver atmoz/sftp  \
