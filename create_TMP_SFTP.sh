2854c353418c19869e3029c44fd81f7af699fca0
Rob <16413104+Kuhlig@users.noreply.github.com>
2019-08-19 22:17:53 +0200

    new sftp server

---
+++ create_TMP_SFTP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker run \
+    -v bpmspace_docker:/home/developer/bpmspace_docker \
+    -v DEV-LIAM2-www-data:/home/developer/LIAM2Server \
+    -v DEV-LIAM2-CLIENT-www-data:/home/developer/LIAM2Client \
+    -p 2222:22 -d --name tmpsftpserver atmoz/sftp  \
+    developer:pass:1001
+docker exec -it tmpsftpserver /bin/sh -c  "chown developer /home/developer/LIAM2* && adduser developer www-data"
+	
+	
+	
