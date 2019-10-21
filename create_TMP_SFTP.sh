8509f6b6f7d305874806088b9212d39805579152
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2019-10-21 11:58:04 +0200

    new TMP_SFTP

--- create_TMP_SFTP.sh
+++ create_TMP_SFTP.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+docker build -t tmpsftpserver_img -f ./_tmp_sftp/Dockerfile .
 docker run \
     -v bpmspace_docker:/home/developer/bpmspace_docker \
     -v DEV-LIAM2-www-data:/home/developer/LIAM2Server \
     -v DEV-LIAM2-CLIENT-www-data:/home/developer/LIAM2Client \
     -v DEV-COMS-CLIENT2-www-data:/home/developer/COMSClient2 \
-    -p 2222:22 -d --name tmpsftpserver atmoz/sftp  \
+    -p 2222:22 -d --name tmpsftpserver tmpsftpserver_img  \
     developer:pass:1001
 docker exec -it tmpsftpserver /bin/sh -c  "chown -R developer /home/developer/* && adduser developer www-data"
 	
